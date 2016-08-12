796d511cc64199858a0d3e157cc3705b9b1bc244
ozomdi <ozz14700@gmail.com>
2016-08-12 19:06:45 +0300

    second commit

--- app/src/main/java/com/petbook/ido/petbook/PetSelectionActivity.java
+++ app/src/main/java/com/petbook/ido/petbook/PetSelectionActivity.java
@@ -1,12 +1,16 @@
 package com.petbook.ido.petbook;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.petbook.ido.petbook.BL.DataLoader;
