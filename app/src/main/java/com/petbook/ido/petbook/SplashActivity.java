b2ed8039ca900e0ad65da1f7e8c8318eee33cda3
ozomdi <ozz14700@gmail.com>
2016-08-12 18:01:45 +0300

    second commit

--- app/src/main/java/com/petbook/ido/petbook/SplashActivity.java
+++ app/src/main/java/com/petbook/ido/petbook/SplashActivity.java
@@ -1,17 +1,47 @@
 package com.petbook.ido.petbook;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class SplashActivity extends ActionBarActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation af = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                iv.startAnimation(af);
+                finish();
+                Intent i = new Intent( getApplicationContext(), MenuActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
     }
+
 }
