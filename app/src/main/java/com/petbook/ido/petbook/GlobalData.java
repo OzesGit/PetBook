7e09dc9f8de729761ac96a10139b7276c8f59322
ozomdi <ozz14700@gmail.com>
2016-08-12 20:05:08 +0300

    second commit

---
+++ app/src/main/java/com/petbook/ido/petbook/GlobalData.java
@@ -0,0 +1,18 @@
+package com.petbook.ido.petbook;
+
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by Ido on 8/12/2016.
+ */
+public class GlobalData {
+
+    public static List<String> strText = new ArrayList<>();
+
+}
