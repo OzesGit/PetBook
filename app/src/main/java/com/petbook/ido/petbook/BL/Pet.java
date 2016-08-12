8cc53012ce97120149212cd3dc948950430b5962
ikiliki <omri96david@gmail.com>
2016-08-12 18:44:40 +0300

    Added omri window

---
+++ app/src/main/java/com/petbook/ido/petbook/BL/Pet.java
@@ -0,0 +1,11 @@
+package com.petbook.ido.petbook.BL;
+
+/**
+ * Created by Omri on 12/08/2016.
+ */
+public class Pet {
+
+
+
+
+}
---
+++ app/src/main/java/com/petbook/ido/petbook/PetSelectionActivity.java
@@ -0,0 +1,48 @@
+package com.petbook.ido.petbook;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.petbook.ido.petbook.BL.DataLoader;
+
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Map;
+
+public class PetSelectionActivity extends ActionBarActivity {
+    private LinearLayout llLayout;
+    private double btnHegihtPercent = 0.15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LoadAnimalTypeList();
+        llLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(llLayout);
+
+//        setContentView(R.layout.activity_pet_selection);
+    }
+
+    private void LoadAnimalTypeList() {
+        Map<String,String> mpAnimalList = DataLoader.GetAnimalTypeList();
+
+        for (String strKey : mpAnimalList .keySet()) {
+            CreateAnimalTypeButton(mpAnimalList .get(strKey));
+        }
+    }
+
+    private void CreateAnimalTypeButton(String strText){
+        Display display = getWindowManager().getDefaultDisplay();
+        Button btn = new Button(this);
+        btn.setText(strText);
+        btn.setHeight((int)(display.getHeight() * btnHegihtPercent));
+
+        llLayout.addView(btn);
+    }
+
+}
