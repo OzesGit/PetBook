be6d88a4c4cc7ca99a1750fb0d9b5835367be35a
ozomdi <ozz14700@gmail.com>
2016-08-12 18:42:07 +0300

    second commit

--- app/src/main/java/com/petbook/ido/petbook/MenuActivity.java
+++ app/src/main/java/com/petbook/ido/petbook/MenuActivity.java
@@ -1,24 +1,26 @@
 package com.petbook.ido.petbook;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class MenuActivity extends ActionBarActivity {
+public class MenuActivity extends Activity {
+
+    RadioButton rbGive;
+    RadioButton rbAdopt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
+        rbGive = (RadioButton) findViewById(R.id.rbGive);
+        rbAdopt = (RadioButton) findViewById(R.id.rbAdopt);
     }
 
     @Override
@@ -37,15 +39,68 @@ public class MenuActivity extends ActionBarActivity {
     }
 
     public void onClickAmuta(View view) {
-        // TODO: Redirect to amuta screen
+
+        if (rbAdopt.isShown() &&
+            rbGive.isShown())
+        {
+            if (!rbAdopt.isChecked() == false &&
+                !rbGive.isChecked() == false )
+            {
+                Toast.makeText(MenuActivity.this, "עליך לבחור אחת מהאופציות", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                // TODO: Redirect to amuta screen
+            }
+        }
+        else
+        {
+            rbAdopt.setVisibility(RadioButton.VISIBLE);
+            rbGive.setVisibility (RadioButton.VISIBLE);
+        }
     }
 
     public void onClickGuest(View view) {
+
+        rbAdopt.setVisibility(RadioButton.INVISIBLE);
+        rbGive.setVisibility (RadioButton.INVISIBLE);
+
     }
 
     public void onClickVet(View view) {
+
+        rbAdopt.setVisibility(RadioButton.INVISIBLE);
+        rbGive.setVisibility (RadioButton.INVISIBLE);
+
     }
 
     public void onClickStore(View view) {
+        rbAdopt.setVisibility(RadioButton.INVISIBLE);
+        rbGive.setVisibility (RadioButton.INVISIBLE);
+    }
+
+    public void onClickRadio(View view) {
+
+        switch (view.getId())
+        {
+            case(R.id.rbAdopt):
+            {
+                if (rbGive.isChecked())
+                {
+                    rbGive.setChecked(false);
+                }
+
+                break;
+            }
+            case(R.id.rbGive):
+            {
+                if (rbAdopt.isChecked())
+                {
+                    rbAdopt.setChecked(false);
+                }
+
+                break;
+            }
+        }
     }
 }
