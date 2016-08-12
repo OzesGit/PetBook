57936280992f4ab29e136ad96a0fa772feafc16f
ozomido <oz14700@gmail.com>
2016-08-12 21:54:20 +0300

    fixed pet hand over screen table layout

--- app/src/main/java/com/petbook/ido/petbook/PetSelectionActivity.java
+++ app/src/main/java/com/petbook/ido/petbook/PetSelectionActivity.java
@@ -1,53 +1,76 @@
 package com.petbook.ido.petbook;
 
+import android.content.res.AssetManager;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
 import com.petbook.ido.petbook.BL.DataLoader;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Dictionary;
 import java.util.List;
 import java.util.Map;
 
 public class PetSelectionActivity extends ActionBarActivity {
     private TableLayout tblLayout;
-    private double btnHegihtPercent = 0.15;
+    private double btnHegihtPercent = 0.25;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tblLayout = new TableLayout(this);
         LoadAnimalTypeList();
         setContentView(tblLayout);
     }
 
     private void LoadAnimalTypeList() {
         Map<String, String> mpAnimalList = DataLoader.GetAnimalTypeList();
-        TableRow row = new TableRow(this);
+        TableRow row = null;
         int colCount = 0;
         for (String strKey : mpAnimalList.keySet()) {
-            row.addView(CreateAnimalTypeButton(mpAnimalList.get(strKey)));
-
             if(colCount%3==0)
             {
+                row = new TableRow(this);
                 tblLayout.addView(row);
             }
+
+            row.addView(CreateAnimalTypeButton(mpAnimalList.get(strKey),strKey));
+            colCount++;
         }
     }
 
-    private Button CreateAnimalTypeButton(String strText) {
+    private Button CreateAnimalTypeButton(String strText,String strKey){
         Display display = getWindowManager().getDefaultDisplay();
         Button btn = new Button(this);
         btn.setText(strText);
         btn.setHeight((int) (display.getHeight() * btnHegihtPercent));
+        btn.setWidth(display.getWidth() / 3);
 
-        return btn;
+        try {
+            AssetManager asmMng = getAssets();
+            InputStream f = asmMng.open(strKey);
+            Drawable d = BitmapDrawable.createFromStream(f, strKey);
+
+            btn.setBackground(d);
+        }
+        catch (Exception ex)
+        {
+
+        }
+        return (btn);
     }
 
-}
+};
