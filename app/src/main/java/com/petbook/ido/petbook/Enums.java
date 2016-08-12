22dcffd9cd8e6b035e37d9089cd73cdbf350e663
ozomido <oz14700@gmail.com>
2016-08-12 20:17:58 +0300

    Added hand over screen and enums class

---
+++ app/src/main/java/com/petbook/ido/petbook/Enums.java
@@ -0,0 +1,12 @@
+package com.petbook.ido.petbook;
+
+/**
+ * Created by Gal on 12/08/2016.
+ */
+public class Enums {
+    public enum Gender {
+        MALE,
+        FEMALE,
+        UNKNOWN
+    }
+}
---
+++ app/src/main/java/com/petbook/ido/petbook/HandOverPetActivity.java
@@ -0,0 +1,13 @@
+package com.petbook.ido.petbook;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class HandOverPetActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hand_over_pet);
+    }
+}
