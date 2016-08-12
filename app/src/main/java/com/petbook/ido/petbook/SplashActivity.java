b674bd9009df6409ff5ddd4f0d55f99953a03165
ozomdi <ozz14700@gmail.com>
2016-08-12 16:43:38 +0300

    second commit

--- app/src/main/java/com/petbook/ido/petbook/SplashActivity.java
+++ app/src/main/java/com/petbook/ido/petbook/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.petbook.ido.petbook;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class SplashActivity extends ActionBarActivity {
 
@@ -9,5 +11,16 @@ public class SplashActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        for (int nIndex = 0; nIndex < 5; nIndex++)
+        {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.startActivity(new Intent(getApplicationContext(), MenuActivity.class));
     }
 }
